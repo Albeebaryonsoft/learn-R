c9309bc2d2e75d0e46c02c255eafd82a64c2c346
QiuYanhang <88499383+QiuYanhang@users.noreply.github.com>
2025-10-28 17:06:39 +0100

    SMALL CHANGES

--- code/barchart.R
+++ code/barchart.R
@@ -4,4 +4,15 @@ counts <- c(24,178,1)
 barplot(counts,main = "Treatarm B",names.arg = c("Alive,no AE","AE","SCT/TKI without prior AE"))
 
 
-ppois(180, lambda = 400)
+# P(N<30)
+ppois(29, lambda = 50)
+
+# P(N>=30)
+1-ppois(29, lambda = 50)
+
+# P(N=30)
+dpois(30, lambda = 50)
+
+
+# P(X≥10)=1−P(X≤9)
+1-ppois(9, lambda = 3)
